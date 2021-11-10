c6e2b421d33dc022db0a2af81a3f5610a9901106
Leo Silva <leonardo.ghost@gmail.com>
2021-11-10 17:25:05 -0300

    add:nodemon

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
+
 const app = express()
 
 
-app.get('/usuario', function (req, res) {
+app.get('/usuarios', function (req, res) {
   res.send(`Rota ativada com GET e recurso usuario: valores de usuarios devem ser retornados`)
 })
 
-app.get('/tarefa', function (req, res) {
+app.get('/tarefas', function (req, res) {
   res.send(`Rota ativada com GET e recurso tarefa: valores de tarefa devem ser retornados`)
 })
 
